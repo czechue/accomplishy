bb6f64eccb09a995e87ce7ab9ca9db397f6ea4f7
Michal Lester <michal.lester@allegro.pl>
2019-05-05 17:38:44 +0200

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js -dont  commit
+module.exports = {
+    googleClientID: '617173654365-vk22uqg17u0qi9e2rnq6k8jhv8dh3oml.apps.googleusercontent.com',
+    googleClientSecret: 'QFyQXhvF3-ltEjSGVIF2Y6RF',
+    mongoURI: 'mongodb+srv://czechue:fender15@accomplishy-xqhwa.mongodb.net/test?retryWrites=true',
+    cookieKey: 'asdadasdqedasdasd'
+};
