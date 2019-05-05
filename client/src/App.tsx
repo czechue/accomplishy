b26677145e237144d0ae289d8d07c29064df990d
Michal Lester <michal.lester@allegro.pl>
2019-05-05 19:45:51 +0200

    production on heroku

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Goals } from "./pages/Goals/Goals";
 const App = () => (
   <Container>
     <Header as="h2">your goals settings</Header>
+      <a href="/auth/google">Sign in with google</a>
     <GoalsPlanner />
   </Container>
 );
