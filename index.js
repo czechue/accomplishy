58740d2e3fd8e8d11728d66fc595eeaa12c274ae
Michal Lester <michal.lester@allegro.pl>
2019-05-05 09:20:42 +0200

    tt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ j: "jjx" });
+  res.send({ j: "jjxxxxxx" });
 });
 
 const PORT = process.env.PORT || 5000;
